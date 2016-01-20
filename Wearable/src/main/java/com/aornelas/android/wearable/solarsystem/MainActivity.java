811c7bf11c3b0ec9a0d4ac05973c1397648c17bc
Andres Ornelas <andres@twitter.com>
2016-01-20 09:33:40 -0800

    Add long-press to exit

--- Wearable/src/main/java/com/aornelas/android/wearable/solarsystem/MainActivity.java
+++ Wearable/src/main/java/com/aornelas/android/wearable/solarsystem/MainActivity.java
@@ -2,9 +2,13 @@ package com.aornelas.android.wearable.solarsystem;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.DotsPageIndicator;
 import android.support.wearable.view.GridViewPager;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnApplyWindowInsetsListener;
 import android.view.WindowInsets;
@@ -12,11 +16,27 @@ import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
+    private DismissOverlayView mDismissOverlay;
+    private GestureDetector mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Resources res = getResources();
+
+        // Obtain the DismissOverlayView element
+        mDismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
+        mDismissOverlay.setIntroText(R.string.long_press_intro);
+        mDismissOverlay.showIntroIfNecessary();
+
+        // Configure a gesture detector
+        mDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            public void onLongPress(MotionEvent ev) {
+                mDismissOverlay.show();
+            }
+        });
+
         final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
         pager.setOnApplyWindowInsetsListener(new OnApplyWindowInsetsListener() {
             @Override
@@ -35,6 +55,12 @@ public class MainActivity extends Activity {
                 // applied since this listener has taken them over.
                 pager.onApplyWindowInsets(insets);
                 return insets;
+            }
+        });
+        pager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return mDetector.onTouchEvent(event);
             }
         });
         pager.setAdapter(new SolarSystemGridPagerAdapter(this, getFragmentManager()));
